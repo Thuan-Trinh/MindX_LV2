22f4ca14a6f11324887290bf1628431eed3b5075
Thuan Trinh <thuantrinh7@gmail.com>
2024-04-27 21:47:52 +0700

    làm đến bài tạp số 10

--- lesson01/practice.js
+++ lesson01/practice.js
@@ -33,7 +33,7 @@
     console.log('Bài 3');
 
     const addPrefix = (list, prefix) => {
-        for (let i= 0; i<list.length; i++){
+        for (let i = 0; i < list.length; i++) {
             list[i] = `${prefix}: ${list[i]}`;
         }
         console.log(list);
@@ -48,11 +48,11 @@
     console.log('Bài 4');
 
     const arr1 = [1, 2, 3, 4];
-    const arr2 = arr1.map(num => num*2);
+    const arr2 = arr1.map(num => num * 2);
     console.log(arr2);
 
     const doubleArr = (arr) => {
-        const result = arr.map(x => x*2);
+        const result = arr.map(x => x * 2);
         console.log(result);
     }
     doubleArr([1, 2, 3, 4]);
@@ -68,9 +68,9 @@
         console.log(resultOddNumber);
         console.log(resultEvenNumber);
     }
-    
+
     sortArr([1, 2, 3, 4]);
-    
+
 }
 
 {
@@ -82,7 +82,7 @@
         { id: 2, name: "Jane", workingDays: 20 },
         { id: 3, name: "Mark", workingDays: 25 },
     ];
-    
+
     const totalWorkingDays = (arr) => {
         let total = 0;
         arr.forEach(employee => {
@@ -91,5 +91,92 @@
         console.log(total);
     }
     totalWorkingDays(employees);
+
+}
+
+{
+    //Bài 7
+    console.log('Bài 7');
+    //Tìm nhân viên có mức lương cao nhất và trả về thông tin của họ
+    const employees = [
+        { id: 1, name: "John", salary: 2000 },
+        { id: 2, name: "Jane", salary: 4000 },
+        { id: 3, name: "Mark", salary: 3000 },
+    ];
+    const findMaxSalary = (arr) => {
+        let maxSalary = arr[0];
+        for (let i = 0; i < arr.length; i++) {
+            arr[i].salary > maxSalary.salary ? maxSalary = arr[i] : maxSalary
+        };
+        console.log(maxSalary);
+    }
+    findMaxSalary(employees);
+}
+
+{
+    //Bài 8
+    console.log('Bài 8');
+    //Tìm nhân viên làm việc chăm chỉ nhất
+    const employees = [
+        { id: 1, name: "John", workingDays: 22, lateDays: 2 },
+        { id: 2, name: "Jane", workingDays: 20, lateDays: 0 },
+        { id: 3, name: "Mark", workingDays: 25, lateDays: 1 },
+    ];
+
+    const findHardWorkingEmployee = (arr) => {
+        let hardWorkingEmployee = arr[0];
+        arr.forEach(employee => {
+            employee.workingDays > hardWorkingEmployee.workingDays ? hardWorkingEmployee = employee : hardWorkingEmployee
+        })
+        console.log(`Nhân viên làm việc chăm chỉ nhất là: `, hardWorkingEmployee);
+    };
+    findHardWorkingEmployee(employees);
+}
+
+{
+    // Bài 9
+    console.log(`Bài 9`);
+    //Tạo một hàm để nhóm các nhân viên theo tên và trả về một object với tên là key và danh sách các nhân viên có cùng tên là value
+    const employees = [
+        { id: 1, name: "John", salary: 2000 },
+        { id: 2, name: "Jane", salary: 2500 },
+        { id: 3, name: "Mark", salary: 3000 },
+        { id: 4, name: "John", salary: 2200 },
+        { id: 5, name: "Mark", salary: 2200 },
+    ];
+
+    const groupEmployeesByName = (employees) => {
+        return employees.reduce((grouped, employee) => {
+            grouped[employee.name] = (grouped[employee.name] || []).concat(employee);
+            return grouped;
+        }, {});
+    }
+    const groupedEmployees = groupEmployeesByName(employees);
+    console.log(groupedEmployees);
+}
+
+{
+    //Bài 10
+    console.log(`Bài 10`);
+    // //Cho một mảng các đối tượng nhân viên, mỗi đối tượng chứa thông tin về số công làm việc, số công đi làm muộn và mức lương của nhân viên.
+    // Tính hiệu suất của mỗi nhân viên bằng cách chia số công làm việc cho mức lương,
+    // sau đó tìm nhân viên có hiệu suất cao nhất và trả về thông tin của họ.
+    const employees = [
+        { id: 1, name: "John", workingDays: 22, lateDays: 2, salary: 2000 },
+        { id: 2, name: "Jane", workingDays: 25, lateDays: 0, salary: 2500 },
+        { id: 3, name: "Mark", workingDays: 20, lateDays: 1, salary: 3000 },
+    ];
+
+    
+    const findMaxPerforEmployee = (arr) => {
+        let maxPerformEmployee = arr[0];
+        let calcMaxPerformEmployee = maxPerformEmployee.workingDays/maxPerformEmployee.salary;
+        arr.forEach(employee => {
+            let perform = employee.workingDays/employee.salary;
+            perform > calcMaxPerformEmployee ? maxPerformEmployee = employee : maxPerformEmployee;
+        })
+        console.log(maxPerformEmployee);
+    };
+    findMaxPerforEmployee(employees);
 
 }
